95d6d0e69df4dce386b6738719a6a9bfec079afe
Andrew Tham <andrewtham@andrews-mbp.lan>
2024-04-26 19:41:49 -0400

    init

--- app/root.tsx
+++ app/root.tsx
@@ -4,7 +4,13 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
+import type { LinksFunction } from '@remix-run/node'
+import stylesheet from '~/tailwind.css?url'
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: stylesheet },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -21,9 +27,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
